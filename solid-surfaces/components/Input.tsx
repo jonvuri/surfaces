6fb3623e8982c891a60da575342482079fa5bde6
Jon Vuri <jrajav@gmail.com>
2023-09-09 21:02:19 -0500

    Add label to input and swap background/text color

--- solid-surfaces/components/Input.tsx
+++ solid-surfaces/components/Input.tsx
@@ -4,11 +4,26 @@ import styles from '../../common/input.module.sass'
 
 type InputProps = {
   primary?: boolean
+  label?: string
   ref?: Ref<HTMLInputElement>
-} & JSX.HTMLAttributes<HTMLInputElement>
+} & JSX.InputHTMLAttributes<HTMLInputElement>
 
-const Input: Component<InputProps> = ({ primary, ref, ...props }) => (
-  <input ref={ref} classList={{ [styles.input]: true, [styles.primary]: primary }} {...props} />
-)
+const Input: Component<InputProps> = ({ primary, label, ref, ...props }) =>
+  label ? (
+    <label>
+      <span class={styles.label}>{label}</span>
+      <input
+        ref={ref}
+        classList={{ [styles.input]: true, [styles.primary]: primary }}
+        {...props}
+      />
+    </label>
+  ) : (
+    <input
+      ref={ref}
+      classList={{ [styles.input]: true, [styles.primary]: primary }}
+      {...props}
+    />
+  )
 
 export default Input
