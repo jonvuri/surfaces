47aad248a66e32b1c16a30b41cbbec9f82dd2d60
Jon Vuri <jrajav@gmail.com>
2023-10-05 22:56:08 -0500

    Add margin prop to headers

--- solid-surfaces/components/typo/Header.tsx
+++ solid-surfaces/components/typo/Header.tsx
@@ -2,18 +2,38 @@ import { ParentComponent } from 'solid-js'
 
 import styles from '../../../common/typo.module.sass'
 
-export const Header: ParentComponent = (props) => (
-  <h1 class={styles.header}>{props.children}</h1>
-)
+type HeaderProps = {
+  margin?: boolean
+}
 
-export const Subheader: ParentComponent = (props) => (
-  <h2 class={styles.subheader}>{props.children}</h2>
-)
+export const Header: ParentComponent<HeaderProps> = (props) => {
+  const margin = props.margin ?? true
+  return (
+    <h1 classList={{ [styles.header]: true, [styles.margin]: margin }}>{props.children}</h1>
+  )
+}
 
-export const ContrastHeader: ParentComponent = (props) => (
-  <h3 class={styles['contrast-header']}>{props.children}</h3>
-)
+export const Subheader: ParentComponent<HeaderProps> = (props) => {
+  const margin = props.margin ?? true
+  return (
+    <h2 classList={{ [styles.subheader]: true, [styles.margin]: margin }}>{props.children}</h2>
+  )
+}
 
-export const GutterHeader: ParentComponent = (props) => (
-  <h4 class={styles['gutter-header']}>{props.children}</h4>
-)
+export const ContrastHeader: ParentComponent<HeaderProps> = (props) => {
+  const margin = props.margin ?? true
+  return (
+    <h3 classList={{ [styles['contrast-header']]: true, [styles.margin]: margin }}>
+      {props.children}
+    </h3>
+  )
+}
+
+export const GutterHeader: ParentComponent<HeaderProps> = (props) => {
+  const margin = props.margin ?? true
+  return (
+    <h4 classList={{ [styles['gutter-header']]: true, [styles.margin]: margin }}>
+      {props.children}
+    </h4>
+  )
+}
