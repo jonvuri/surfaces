852cdd014c94763ca5677a057b0b2f3e79a59d24
Jon Vuri <jrajav@gmail.com>
2023-10-07 10:15:23 -0500

    Update Table styling and add rowKey

--- solid-surfaces/components/Table.tsx
+++ solid-surfaces/components/Table.tsx
@@ -8,42 +8,50 @@ type ColumnSpec = {
   key: string
   name: string
 }
-
 type ColumnData = string | number | boolean | null | undefined
-
 type Row = Record<string, ColumnData>
 
 type TableProps = {
   columns: ColumnSpec[]
   data: Row[]
+  rowKey?: string
+  cellRenderer?: (cell: ColumnData, column: ColumnSpec, id: string) => JSX.Element
   header?: boolean
 } & JSX.HTMLAttributes<HTMLTableElement>
 
-const Table: Component<TableProps> = (props) => (
-  <table class={styles['table']} {...props}>
-    {(props.header ?? true) && (
-      <thead>
-        <tr>
-          <For each={props.columns}>{(column) => <th>{column.name}</th>}</For>
-        </tr>
-      </thead>
-    )}
-    <tbody>
-      <For each={props.data}>
-        {(row) => (
+const Table: Component<TableProps> = (props) => {
+  const cellRenderer = props.cellRenderer ?? ((cell) => <Boxed>{cell}</Boxed>)
+
+  return (
+    <table class={styles['table']} {...props}>
+      {(props.header ?? true) && (
+        <thead>
           <tr>
-            <For each={props.columns}>
-              {(column) => (
-                <td>
-                  <Boxed>{row[column.key]}</Boxed>
-                </td>
-              )}
-            </For>
+            <For each={props.columns}>{(column) => <th>{column.name}</th>}</For>
           </tr>
-        )}
-      </For>
-    </tbody>
-  </table>
-)
+        </thead>
+      )}
+      <tbody>
+        <For each={props.data}>
+          {(row) => (
+            <tr>
+              <For each={props.columns}>
+                {(column) => (
+                  <td>
+                    {cellRenderer(
+                      row[column.key],
+                      column,
+                      String(row[props.rowKey || 'id'] || ''),
+                    )}
+                  </td>
+                )}
+              </For>
+            </tr>
+          )}
+        </For>
+      </tbody>
+    </table>
+  )
+}
 
 export default Table
