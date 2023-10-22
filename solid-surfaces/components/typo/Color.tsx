147d83bf180e4b0865ea065515317f63314b854f
Jon Vuri <jrajav@gmail.com>
2023-10-22 11:22:33 -0500

    Add Shimmer with hardcoded teal/green gradient

--- solid-surfaces/components/typo/Color.tsx
+++ solid-surfaces/components/typo/Color.tsx
@@ -2,6 +2,10 @@ import { ParentComponent } from 'solid-js'
 
 import styles from '../../../common/typo.module.sass'
 
+export const Shimmer: ParentComponent = (props) => (
+  <div class={styles['shimmer']}>{props.children}</div>
+)
+
 export const Dimmed: ParentComponent = (props) => (
   <div class={styles['dimmed']}>{props.children}</div>
 )
